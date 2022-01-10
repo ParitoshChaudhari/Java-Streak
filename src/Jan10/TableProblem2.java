6443ba46eef6d24fdab808a25b189d859dd98d42
Paritosh Chaudhari <chaudhariparitosh9571@gmail.com>
2022-01-10 20:40:50 +0530

    10 January 2022 work done

---
+++ src/Jan10/TableProblem2.java
@@ -0,0 +1,13 @@
+package Jan10;
+
+public class TableProblem2 {
+    public static void main(String[] args) {
+        ////CWH JAVA CHAPTER 5 Question 4
+        //Write a program to print a multiplication table of 10 in reverse order
+
+        //loop
+        for(int i = 10 ; i >= 1 ; i--){
+            System.out.println("10 * " + i + " = " + (10*i));
+        }
+    }
+}
